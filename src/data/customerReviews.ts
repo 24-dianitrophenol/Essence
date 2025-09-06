bba9a60ae1f78704437f5a90495ff7bbeacb2ea5
BANGOLE ALVIN <bongolealvin24@gmail.com>
2025-09-06 16:21:55 +0300

    upgrade two

---
+++ src/data/customerReviews.ts
@@ -0,0 +1,45 @@
+// Easy-to-edit Customer Reviews data structure
+// To modify reviews: simply edit the name, profession, image, rating, and comment fields below
+
+export const customerReviews = [
+  {
+    id: 1,
+    name: 'Zimula Farid',
+    profession: 'Techpreneur',
+    image: '/images/My image.png',
+    rating: 5,
+    comment: 'Absolutely love these products! My skin has never felt better.'
+  },
+  {
+    id: 2,
+    name: 'Bangole Alvin',
+    profession: 'Software Engineer',
+    image: '/images/Alvin image.png',
+    rating: 4,
+    comment: 'High quality and natural. I recommend to all my friends.'
+  },
+  {
+    id: 3,
+    name: 'Farahan Vicent',
+    profession: 'Entrepreneur',
+    image: '/images/lapter-BaH3qKJV.jpg',
+    rating: 5,
+    comment: 'The best skincare I have ever used! Will buy again.'
+  },
+  {
+    id: 4,
+    name: 'Michael R.',
+    profession: 'Doctor',
+    image: '/images/White man.jpeg',
+    rating: 5,
+    comment: 'As a medical professional, I appreciate the natural ingredients and effectiveness.'
+  },
+  {
+    id: 5,
+    name: 'Grace M.',
+    profession: 'Entrepreneur',
+    image: '/images/White woman.jpeg',
+    rating: 4,
+    comment: 'Great value for money and amazing results on my acne-prone skin.'
+  }
+];
